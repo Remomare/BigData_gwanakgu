e77f8371eb589de7a318c7fdbc9bd09917c7db07
Remomare <75660801+Remomare@users.noreply.github.com>
2023-05-12 17:10:37 +0900

    add parser

--- main.py
+++ main.py
@@ -7,26 +7,26 @@ import pharmacy_data_pre
 import argparse
 import map
 
+def visualizaition_pharmacy_location(args):
+    
 
 
-
-
-
-
-
+    return
 
 
 def main(args):
 
-    pharmacy_location_data = pharmacy_data_pre.pharmacy_data(args)
-
-    print(pharmacy_location_data)
-    map_pharmacy = map.pharmacy_gwnakgu_mapping(args, pharmacy_location_data)
-
-    map_pharmacy.save("./test.html")
-
-    webbrowser.open("test.html")
     
+    if args.visualization_pharmacy == True:
+        pharmacy_location_data = pharmacy_data_pre.pharmacy_data(args)
+
+        print(pharmacy_location_data)
+
+        map_pharmacy = map.pharmacy_gwnakgu_mapping(args, pharmacy_location_data)
+
+        map_pharmacy.save(args.pharmacy_location_map_path)
+
+        webbrowser.open(args.pharmacy_location_map_file_name)
 
     return
 
@@ -36,6 +36,13 @@ if __name__ == "__main__":
 
     parser.add_argument('--pharmacy_data_path', default = './pharmacy_regulatory_gwnakgu.csv', type=str, 
                         help='pharmacy data path')
+    parser.add_argument('--pharmacy_location_map_file_name', default='location_of_pharmacy.html', type=str)
+    parser.add_argument('--pharmacy_location_map_path', default='./location_of_pharmacy.html', type=str,
+                        help="pharmacy location map path")
+    
+
+    parser.add_argument('--visualization_pharmacy', default=True, type=bool )
+
 
     args = parser.parse_args()
 

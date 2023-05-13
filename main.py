09ed4942d44f4bfcdd348bea5a82ae337549aa8f
Remomare <75660801+Remomare@users.noreply.github.com>
2023-05-13 10:59:43 +0900

    add visulization function

--- main.py
+++ main.py
@@ -5,7 +5,15 @@ import folium
 import webbrowser
 import data_preprocessing
 import argparse
+import pandas as pd
 import map
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
+import numpy as np
+import sklearn
+import seaborn as sns
+import statsmodels.api as sm
+from statsmodels.formula.api import ols
 
 
 def main(args):
@@ -24,10 +32,55 @@ def main(args):
 
     if args.visulization_population_of_gwanakgu_by_dong == True:
         population_of_gwankgu_by_dong  = data_preprocessing.population_of_gwanakgu_by_dong_data(args)
+        
+        x = np.arange(21)
+        y = pd.to_numeric(population_of_gwankgu_by_dong['인구수(계)'].str.replace(',', ''))
+
+        plt.rc('font', family='NanumSquareRoundOTF')
+        plt.rc('xtick', labelsize=8)
+        plt.figure(figsize = (12,5))
+        plt.bar(x, y, width=0.8)
+        plt.title("관악구 행정동 별 인구수")
+        plt.ylabel("인구수")
+        plt.xticks(x, population_of_gwankgu_by_dong['행정기관'].to_list())
+
+        print(plt.ylim())
+        plt.savefig("bar graph.png")    
+
+    if args.visulization_number_of_pharmacy == True:
         number_of_pharmacy_by_dong = data_preprocessing.pharmacy_location_by_dong_data(args)
 
-        #print(population_of_gwankgu_by_dong)
-        print(number_of_pharmacy_by_dong)
+
+    if args.Linear_Regression== True:
+        #Linear Regression
+        # y= 75*x  Reference Line
+        # x = 약사 수 = 약국 수 (약국당 약사 한명만 있다고 가정) #하루 적정 약사당 조제건수 75 #y: 총 조제건수= 수용가능 인구
+        #y_dong : 연별 관악구 합계 인구수 #x_dong = 연별 약국(=약사) 수
+
+        population_data_set = data_preprocessing.dapopulation_of_gwanakgu(args)
+
+        x_dong = population_data_set.loc['관악구']
+        y_dong = 0
+
+        x = 0
+
+        plot_data = [y_dong,x_dong]
+
+        model = sklearn.linear_model.LinearRegression()    
+        model.fit(plot_data[["y_dong"]], plot_data[["x_dong"]]) 
+
+        r_sq = model.score(plot_data[["y_dong"]], plot_data[["x_dong"]])
+        print(model.intercept_) 
+        print(model.coef_)  
+
+        sns.relplot(x= "x_dong", y = "y_dong", data = plot_data)
+        sns.lineplot(x= "x_dong", y = "y_dong", color="r", data = plot_data) 
+        plt.plot(y=75*x) #reference line y = 75*x
+
+        #One-Way Anova
+        model = ols('y_dong ~ x_dong', data= plot_data).fit() #ols ordinary least square
+        sm.stats.anova_lm(model) 
+    
 
 
     return
@@ -49,6 +102,10 @@ if __name__ == "__main__":
 
     parser.add_argument('--visualization_pharmacy', default=False, type=bool )
     parser.add_argument('--visulization_population_of_gwanakgu_by_dong', default= True, type=bool)
+
+    parser.add_argument('--population_of_gwanakgu_file_path', default='./population_of_gwanakgu')
+
+    parser.add_argument('--Linear_Regression', default=False)
 
 
     args = parser.parse_args()
